ae88c06a9a9ef2d5508a881847f2d715cdeb435d
Marco Cafasso <marcocafassog@gmail.com>
2022-11-17 15:39:41 +0100

    feat: add a product list into the Main

--- java-oop-shop/generation/italy/shop/Main.java
+++ java-oop-shop/generation/italy/shop/Main.java
@@ -6,20 +6,38 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		/*
+		 * Metodo per far inserire i dati all'utente 
+		 * 
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("Nome prodotto: ");
+			String nome = sc.nextLine();
+			
+			System.out.print("Descrizione prodotto: ");
+			String descrizione = sc.nextLine();
+			
+			System.out.print("Prezzo prodotto: ");
+			int prezzo = sc.nextInt();
+			
+			Prodotto p = new Prodotto(nome, descrizione, prezzo);
+			System.out.println(p);
+		*
+		*/
 		
-		System.out.print("Nome prodotto: ");
-		String nome = sc.nextLine();
+		Prodotto prodotto1 = new Prodotto(
+				"FENDER Squier Classic Vibe '70s Telecaster Deluxe",
+				"La Telecaster Deluxe anni '70 Classic Vibe è un fedele cenno all'evoluzione della Tele degli anni '70, che crea un suono imponente grazie ai doppi pickup humbucker Wide Range progettati da Fender.",
+				420);
 		
-		System.out.print("Descrizione prodotto: ");
-		String descrizione = sc.nextLine();
+		Prodotto prodotto2 = new Prodotto(
+				"FENDER Squier FSR Affinity",
+				"Una superba porta d'accesso all'antica famiglia Fender, la Squier Affinity Series Telecaster HH offre un design leggendario ed il suono per eccellenza degli aspiranti eroi della chitarra di oggi.",
+				299);
 		
-		System.out.print("Prezzo prodotto: ");
-		int prezzo = sc.nextInt();
-		
-		Prodotto p = new Prodotto(nome, descrizione, prezzo);
-		System.out.println(p);
-
+		System.out.println(prodotto1.toString());
+		System.out.println();
+		System.out.println(prodotto2.toString());
 	}
 
 }
